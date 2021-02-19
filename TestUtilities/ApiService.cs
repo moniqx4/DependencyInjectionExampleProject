af2d3856ca78dbabfe7b6b7e64147adb1c3c6158
Monica D <moniqx4@gmail.com>
2021-02-19 08:31:36 -0700

    added API Client and TestUtilties project

---
+++ TestUtilities/ApiService.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+
+namespace TestUtilities
+{
+    public class ApiService
+    {
+        public ApiService()
+        {
+
+        }
+
+        public IList<T> GetApiResponse(string apiEndpoint)
+        {
+            var client = new RestClient(apiEndpoint);
+            var request = new RestRequest
+            {
+                Method = Method.GET,
+                RequestFormat = DataFormat.Json
+            };
+
+            var response = client.Execute(request);
+
+            if(response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                throw new SystemException("Call to endpoint failed" + response.StatusCode);
+            }
+
+            return JsonConvert.DeserializeObject<IList<T>>(response.Content);
+        }
+    }
+
+    public delegate void T();
+}
